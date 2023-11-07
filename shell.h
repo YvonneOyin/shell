d95e9de105ee697b4c5d8af75cb7184871d52420
YvonneOyin <olufemioyindamola37@gmail.com>
2023-11-07 17:52:53 +0100

    Updated the header file...

--- shell.h
+++ shell.h
@@ -3,5 +3,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+extern char **environ;
+
+int _putchar(char);
+void _puts(char *);
+
+/* string manipulation */
+int _strlen(char *);
+char *_strcpy(char *, char *);
+int _strcmp(char *, char *);
+char *_strcat(char *, char *);
+char *_strchr(char *, char);
+
+void prompt(__attribute__((unused))int);
+
 
 #endif /* _SHELL_H_ */
